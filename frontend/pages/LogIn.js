0ba93d8a0a84edbb000da4e461b8f3c2295eca1f
Justin Kim <naznsan@gmail.com>
2021-02-18 17:37:16 +1300

    Add log in button

--- frontend/pages/LogIn.js
+++ frontend/pages/LogIn.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Head from 'next/head';
 import Navbar from '../components/navbar/Navbar';
+import styles from '../styles/LogIn.module.css';
 
 const LogIn = () => {
   return (
-    <div>
+    <div className={styles.root}>
       <Head>
         <title>Log In</title>
       </Head>
@@ -18,6 +19,7 @@ const LogIn = () => {
         <p>Password: </p>
         <input type='password'></input>
       </div>
+      <button type='submit'>Log In</button>
     </div>
   );
 }
